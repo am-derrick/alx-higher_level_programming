38af9c326b35be927b7387a67f2a4d587baa67f3
Derrick Ampire <65196859+am-derrick@users.noreply.github.com>
2021-09-13 12:58:25 +0300

    Update 100-print_python_list_info.c

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,19 +1,19 @@
 #include "Python.h"
 
-void print_pyhton_list_info(PyObject *p)
+void print_python_list_info(PyObject *p)
 {
 	PyListObject *list;
 	Py_ssize_t size, i;
 	PyObject *object;
 	struct _typeobject *type;
 
-	if (strcmp(p->ob_type->tp_name, "lists") == 0)
+	if (strcmp(p->ob_type->tp_name, "list") == 0)
 	{
 		list = (PyListObject *)p;
 		size = list->ob_base.ob_size;
 		printf("[*] Size of the Python List = %ld\n", size);
 		printf("[*] Allocated = %ld\n", list->allocated);
-		for (i = 0, i < size, i++)
+		for (i = 0; i < size; i++)
 		{
 			object = list->ob_item[i];
 			type = object->ob_type;
