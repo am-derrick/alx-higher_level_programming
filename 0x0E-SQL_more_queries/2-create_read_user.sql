929d8d1c1e32e04319726eec0c4c37b2248bf278
Derrick Ampire <ampire90@gmail.com>
2021-11-17 17:43:04 -0800

    First files

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,7 @@
+-- creates database hbtn_0d_2 and user user_0d_2
+-- user_0d_2 should have only SELECT privileges, password set to user_0d_2_pwd
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXITS user_0d_2@localhost IDENTIFIED BY user_0d_2_pwd;
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
+FLUSH PRIVILEGES;
