7745748b8eeee4b91ba444be57e894e193d008e8
Derrick Ampire <65196859+am-derrick@users.noreply.github.com>
2021-11-18 04:51:21 +0300

    Update 1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- creates user user_0d_1
 -- password set to user_0d_1_pwd
 
-CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY user_0d_1_pwd;
-GRANT PRIVILEGES *.* TO user_0d_1@localhost;
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT PRIVILEGES *.* TO 'user_0d_1'@'localhost';
