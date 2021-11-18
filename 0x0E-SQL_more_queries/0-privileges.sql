e9886cb9afae200552e185d845c32c4e22ae3e47
Derrick Ampire <65196859+am-derrick@users.noreply.github.com>
2021-11-18 04:46:36 +0300

    Update 0-privileges.sql

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- lists all privileges of user_0d_1 and user_0d_2 in localhost
 
-SHOW GRANTS FOR user_0d_1@localhost;
-SHOW FRANTS FOR user_0d_2@localhost;
+SHOW GRANTS FOR 'user_0d_1@localhost';
+SHOW FRANTS FOR 'user_0d_2@localhost';
