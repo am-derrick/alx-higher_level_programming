53dc4665b3617170fdff0dd2a7cc3846f120a365
Derrick Ampire <65196859+am-derrick@users.noreply.github.com>
2021-12-15 02:46:50 +0300

    Update 101-sorted.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -3,7 +3,7 @@ let dict = require('./101-data.js').dict;
 let newDict = {};
 for (let key in dict) {
   if (newDict[dict[key]] === undefined) {
-    newDict[dict[key]] == [];
+    newDict[dict[key]] = [];
   }
   newDict[dict[key]].push(key);
 }
