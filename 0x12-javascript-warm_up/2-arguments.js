32534e4349829b2fddcdc8c02ecf951990f4c8ee
Derrick Ampire <65196859+am-derrick@users.noreply.github.com>
2021-12-14 09:06:34 +0300

    Reassigned variable

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 'use strict';
-let len = process.argv.length;
-if (len === 2) {
+let l = process.argv.length;
+if (l === 2) {
   console.log('No argument');
-} else if (len === 3) {
+} else if (l === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
