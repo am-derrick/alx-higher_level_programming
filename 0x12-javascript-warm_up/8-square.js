3f7335cc8b04397bb45c22ae6c8fd5a5e3b9b3f2
Derrick Ampire <65196859+am-derrick@users.noreply.github.com>
2021-12-14 05:48:17 +0300

    Sign changed

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 'use strict';
-let val - process.argv[2];
+let val = process.argv[2];
 if (isNan(val)) {
   console.log('Missing size');
 } else {
   for (let i = 0; i < val; i++) {
     console.log('X'.repeat(val));
+  }
 }
