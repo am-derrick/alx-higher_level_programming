9764d02d446eccd6c7fddaf6a4a31d16410074fd
Derrick Ampire <65196859+am-derrick@users.noreply.github.com>
2021-12-14 05:50:10 +0300

    isNan corrected to is NaN

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -3,7 +3,7 @@
 let a = process.argv[2];
 let b = process.argv[3];
 function add (a, b) {
-  if (isNaN(a) || isNan(b)) {
+  if (isNaN(a) || isNaN(b)) {
     return (NaN);
   } else {
     return (parseInt(a) + parseInt(b));
