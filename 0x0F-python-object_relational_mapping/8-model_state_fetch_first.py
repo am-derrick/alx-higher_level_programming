c4413891b5d5fea238f095e2177bfcbcc95f0181
Derrick Ampire <65196859+am-derrick@users.noreply.github.com>
2021-12-19 15:49:09 +0300

    create_engine updated

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -9,7 +9,7 @@ from sqlalchemy import create_engine
 from sys import argv
 from model_state import Base, State
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
                                                                         argv[2],
                                                                         argv[3]))
