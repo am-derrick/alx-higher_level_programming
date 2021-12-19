775a514a0e4f1177e3231322d1a56671c836ff03
Derrick Ampire <65196859+am-derrick@users.noreply.github.com>
2021-12-19 15:45:46 +0300

    Metadata updated

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -13,11 +13,11 @@ if __name__ = "__main__":
     engine = create_engine('mysql+ mysqldb://{}:{}@localhost/{}'.format(argv[1],
                                                                         argv[2],
                                                                         argv[3]))
-    Base.metadat.create_all(engine)
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
     first_state = session.query(State).order_by(State.id).first()
-    if first_state is no None:
+    if first_state is not None:
         print("{}: {}".format(first_state.id, first_state.name))
     else:
         print("Nothing")
