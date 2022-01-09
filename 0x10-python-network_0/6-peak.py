4aab544d11fc56ccc91c4699814043639633a869
Derrick Ampire <65196859+am-derrick@users.noreply.github.com>
2022-01-09 17:33:09 +0300

    pep fixed

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -9,7 +9,7 @@ def find_peak(list_of_integers):
     if l == 0:
         return
     mid = l // 2
-    if (mid = l - 1 or list_int[mid] >= list_int[mid + 1]) and (mid == 0 or list_int[mid] >= list_int[mid -1]):
+    if (mid == l - 1 or list_int[mid] >= list_int[mid + 1]) and (mid == 0 or list_int[mid] >= list_int[mid - 1]):
         return list_int[mid]
     if mid != l - 1 and list_int[mid + 1] > list_int[mid]:
         return find_peak(list_int[m + 1:])
